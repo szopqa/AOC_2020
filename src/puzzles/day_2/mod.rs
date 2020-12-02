ab17f980919150e6d32e6e42662279bc1b0a9de2
szopqa96@gmail.com <szopqa96@gmail.com>
2020-12-02 15:49:01 +0100

    Added solution for day_2

---
+++ src/puzzles/day_2/mod.rs
@@ -0,0 +1,119 @@
+use crate::puzzles::solution::Solution;
+
+use regex::Regex;
+
+pub struct Puzzle {}
+
+
+#[derive(Debug)]
+struct PassValidator {
+    _min_num: u8,
+    _max_num: u8,
+    _letter: char,
+    _pass: String
+}
+
+impl PassValidator {
+    pub fn new(_pass_line: &str) -> Self {
+        lazy_static! {
+            static ref MIN_REGEX: Regex = Regex::new(r"^\d+").unwrap();
+            static ref MAX_REGEX: Regex = Regex::new(r"^\d+-(\d+)").unwrap();
+            static ref LETTER_REGEX: Regex = Regex::new(r"([a-z]+):").unwrap();
+            static ref PASS_REGEX: Regex = Regex::new(r":\s+(\w+)").unwrap();
+        }
+
+        Self {
+            _min_num: MIN_REGEX.captures(_pass_line).unwrap().get(0).unwrap().as_str().parse::<u8>().unwrap(),
+            _max_num: MAX_REGEX.captures(_pass_line).unwrap().get(1).unwrap().as_str().parse::<u8>().unwrap(),
+            _letter: LETTER_REGEX.captures(_pass_line).unwrap().get(1).unwrap().as_str().parse::<char>().unwrap(),
+            _pass: PASS_REGEX.captures(_pass_line).unwrap().get(1).unwrap().as_str().parse::<String>().unwrap()
+        }
+    }
+
+    pub fn is_valid_part_one(&self) -> bool {
+        let mut _occurrences = 0;
+
+        for _each_letter in self._pass.chars() {
+            if _each_letter == self._letter {
+                _occurrences += 1;
+            }
+        }
+
+        _occurrences >= self._min_num && _occurrences <= self._max_num
+    }
+
+    pub fn is_valid_part_two(&self) -> bool {
+        let mut _occurrences = 0;
+
+        let _at_min = self._pass.chars().nth(self._min_num as usize - 1).unwrap();
+        let _at_max = self._pass.chars().nth(self._max_num as usize - 1).unwrap();
+
+        (_at_min == self._letter && _at_max != self._letter) ||
+            (_at_min != self._letter && _at_max == self._letter)
+    }
+}
+
+impl Solution for Puzzle {
+    type PuzzleInput = String;
+    type OutputPartOne = usize;
+    type OutputPartTwo = usize;
+
+    fn solve_part_one(_input: &Vec<Self::PuzzleInput>) -> Self::OutputPartOne {
+        let mut _result: Self::OutputPartOne = 0;
+
+        _input
+            .iter()
+            .map(|_pass| PassValidator::new(_pass))
+            .filter(|_v| _v. is_valid_part_one())
+            .collect::<Vec<PassValidator>>()
+            .len()
+    }
+
+    fn solve_part_two(_input: &Vec<Self::PuzzleInput>) -> Self::OutputPartTwo {
+        let mut _result: Self::OutputPartTwo = 0;
+
+        _input
+            .iter()
+            .map(|_pass| PassValidator::new(_pass))
+            .filter(|_v| _v. is_valid_part_two())
+            .collect::<Vec<PassValidator>>()
+            .len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzles::day_2::*;
+
+    #[test]
+    fn test_part_one() {
+        // given
+        let _input = vec![
+            "1-3 a: abcde",
+            "1-3 b: cdefg",
+            "2-9 c: ccccccccc"
+        ].into_iter().map(|_i| String::from(_i)).collect();
+
+        // when
+        let _res: usize = Puzzle::solve_part_one(&_input);
+
+        // then
+        assert_eq!(_res, 2);
+    }
+
+    #[test]
+    fn test_part_two() {
+        // given
+        let _input = vec![
+            "1-3 a: abcde",
+            "1-3 b: cdefg",
+            "2-9 c: ccccccccc"
+        ].into_iter().map(|_i| String::from(_i)).collect();
+
+        // when
+        let _res: usize = Puzzle::solve_part_two(&_input);
+
+        // then
+        assert_eq!(_res, 1);
+    }
+}
--- src/puzzles/mod.rs
+++ src/puzzles/mod.rs
@@ -2,7 +2,9 @@ pub mod solution;
 use super::puzzles::solution::Solution;
 
 pub mod day_1;
+pub mod day_2;
 
 pub fn solve_all() {
     day_1::Puzzle::solve("day_1");
+    day_2::Puzzle::solve("day_2");
 }
