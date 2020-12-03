77aeb4b4bdb66ecb59729e00c5797c8ca2a8ed2a
szopqa <szopqa96@gmail.com>
2020-12-03 16:55:46 +0100

    day_3 initial implementation

--- src/puzzles/mod.rs
+++ src/puzzles/mod.rs
@@ -3,8 +3,10 @@ use super::puzzles::solution::Solution;
 
 pub mod day_1;
 pub mod day_2;
+pub mod day_3;
 
 pub fn solve_all() {
     day_1::Puzzle::solve("day_1");
     day_2::Puzzle::solve("day_2");
+    day_3::Puzzle::solve("day_3");
 }
