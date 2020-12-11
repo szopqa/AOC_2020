b0d46c113903a2102d4926515d87957d61bc36d5
szopqa96@gmail.com <szopqa96@gmail.com>
2020-12-11 20:50:07 +0100

    Added solution for day_11 p2

--- src/puzzles/day_11/mod.rs
+++ src/puzzles/day_11/mod.rs
@@ -45,16 +45,49 @@ impl Ferry {
         neighbours
     }
 
+    fn get_far_neighbours(&self, x_pos: &usize, y_pos: &usize) -> Vec<(usize, usize, char)> {
+        let mut neighbours: Vec<(usize, usize, char)> = vec![];
+
+        let x_incomes:Vec<isize> = if *x_pos > 0 { vec![-1, 0, 1] } else { vec![0, 1] };
+        let y_incomes:Vec<isize> = if *y_pos > 0 { vec![-1, 0, 1] } else { vec![0, 1] };
+
+        for a in &x_incomes {
+            for b in &y_incomes {
+                let mut x = (*x_pos as isize + a) as usize;
+                let mut y = (* y_pos as isize + b) as usize;
+
+                loop {
+                    if x > self.seats.len()-1 || y > self.seats.get(0).unwrap().len()-1 {
+                        break;
+                    }
+
+                    let n =  &self.seats[x][y];
+                    if *n != '.' {
+                        if *x_pos != x || *y_pos != y {
+                            neighbours.push((x, y, *n));
+                        }
+                        break;
+                    }
+
+                    x = (x as isize + a) as usize;
+                    y = (y as isize + b) as usize;
+                }
+            }
+        }
+
+        neighbours
+    }
+
     fn can_be_occupied(neighbours: &Vec<(usize, usize, char)>) -> bool {
         neighbours.iter()
             .filter(|(_, _, seat)| *seat == '#')
             .count() == 0
     }
 
-    fn can_be_emptied(neighbours: &Vec<(usize, usize, char)>) -> bool {
+    fn can_be_emptied(neighbours: &Vec<(usize, usize, char)>, min_num: usize) -> bool {
         neighbours.iter()
             .filter(|(_, _, seat)| *seat == '#')
-            .count() >= 4
+            .count() >= min_num
     }
 
     pub fn shuffle_seats(&mut self) {
@@ -69,7 +102,34 @@ impl Ferry {
                         seats[row_pos][pos_in_row] = '#';
                         changed_state = true;
                     }
-                    if *each_seat != 'L' && Self::can_be_emptied(&neighbours) {
+                    if *each_seat != 'L' && Self::can_be_emptied(&neighbours, 4) {
+                        seats[row_pos][pos_in_row] = 'L';
+                        changed_state = true;
+                    }
+                }
+            }
+        }
+
+        if !changed_state {
+            self.seats_state_stable = true
+        }
+
+        self.seats = seats;
+    }
+
+    pub fn shuffle_far_seats(&mut self) {
+        let mut seats = self.get_seats_copy();
+
+        let mut changed_state = false;
+        for (row_pos, each_row) in self.seats.iter().enumerate() {
+            for (pos_in_row, each_seat) in each_row.iter().enumerate() {
+                if *each_seat != '.' {
+                    let neighbours = self.get_far_neighbours(&row_pos, &pos_in_row);
+                    if *each_seat != '#' && Self::can_be_occupied(&neighbours) {
+                        seats[row_pos][pos_in_row] = '#';
+                        changed_state = true;
+                    }
+                    if *each_seat != 'L' && Self::can_be_emptied(&neighbours, 5) {
                         seats[row_pos][pos_in_row] = 'L';
                         changed_state = true;
                     }
@@ -120,7 +180,18 @@ impl Solution for Puzzle {
     }
 
     fn solve_part_two(input: &Vec<Self::PuzzleInput>) -> Self::OutputPartTwo {
-        0
+        let mut seats: Vec<Vec<char>> = vec![vec![]; input.len()];
+        for (i, each_row) in input.iter().enumerate() {
+            seats[i] = each_row.chars().map(|c| c).collect();
+        }
+
+        let mut ferry = Ferry::new(seats);
+
+        while !ferry.seats_state_stable {
+            ferry.shuffle_far_seats();
+        }
+
+        ferry.get_occupied()
     }
 }
 
@@ -149,5 +220,28 @@ mod tests {
 
         // then
         assert_eq!(_res, 37);
+    }
+
+    #[test]
+    fn test_part_two() {
+        // given
+        let _input = vec![
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL",
+        ].into_iter().map(|_i| String::from(_i)).collect();
+
+        // when
+        let _res: u64 = Puzzle::solve_part_two(&_input);
+
+        // then
+        assert_eq!(_res, 26);
     }
 }
